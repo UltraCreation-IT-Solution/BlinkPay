106f6719785355497e7206a757237236b1b26c7d
rishabh2023 <rishabh2023@gmail.com>
2021-08-11 19:26:41 +0530

    ok

--- pay/views.py
+++ pay/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from .models import Payment_details
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import *
 # Create your views here.
 class Show_Payment(APIView):
     def get(self,request):
         data = Payment_details.objects.all()
         serialize = PaymentSerializer(data,many = True)
-        return Response(serialize.data)
+        return Response(data = serialize.data,status = status.HTTP_200_OK)
