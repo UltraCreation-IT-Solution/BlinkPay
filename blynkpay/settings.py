f874783858cce3910b0d10f85f4f76d336cb6c77
rishabh2023 <rishabh2023@gmail.com>
2021-08-29 22:38:01 +0530

    final

--- blynkpay/settings.py
+++ blynkpay/settings.py
@@ -12,12 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+os.environ['DISPLAY']=':0'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
